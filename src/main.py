3d7108855e29fe7e2ab07248a737bf6297b3ba6f
Uriel148 <cardenasau2018@licifug.ugto.mx>
2025-03-07 22:26:06 -0600

    Ahora se pueden cambiar varias palabras pero por la misma palabra

--- src/main.py
+++ src/main.py
@@ -1,12 +1,6 @@
 from docx import Document
 
-def reemplazar_palabra_en_docx(documento_entrada,palabra_buscar,palabra_reemplazo):
-    # Solicitar datos al usuario
-    #documento_entrada = input("Ingresa la ruta del documento Word (.docx): ")
-    #palabra_buscar = input("Ingresa la palabra a buscar (ej. XXXXX): ")
-    #palabra_reemplazo = input("Ingresa la palabra de reemplazo: ")
-    #documento_salida = input("Ingresa el nombre para el nuevo documento: ")
-
+def reemplazar_palabra_en_docx(documento_entrada,palabras_buscar,palabra_reemplazo):
     # Cargar el documento
     doc = Document(documento_entrada)
     
@@ -14,8 +8,9 @@ def reemplazar_palabra_en_docx(documento_entrada,palabra_buscar,palabra_reemplaz
     def reemplazar_en_elemento(elemento):
         if hasattr(elemento, 'runs'):
             for run in elemento.runs:
-                if palabra_buscar in run.text:
-                    run.text = run.text.replace(palabra_buscar, palabra_reemplazo)
+                for palabra_buscar in palabras_buscar:
+                    if palabra_buscar in run.text:
+                        run.text = run.text.replace(palabra_buscar, palabra_reemplazo)
     
     # Procesar párrafos principales
     for párrafo in doc.paragraphs:
@@ -46,8 +41,8 @@ def reemplazar_palabra_en_docx(documento_entrada,palabra_buscar,palabra_reemplaz
     print(f"\nDocumento modificado guardado como: {documento_salida}")
 
 if __name__ == "__main__":
-    documento_entrada = r"doc\test.docx"
-    palabra_buscar = "XXXXX"
+    documento_entrada = r"doc\test2.docx"
+    palabras_buscar = ["XXXXX","YYYYY","ZZZZZ"] 
     palabras_reemplazo = ["exitosa_1","exitosa_2","exitosa_3"]
     for palabra_reemplazo in palabras_reemplazo: 
-        reemplazar_palabra_en_docx(documento_entrada,palabra_buscar,palabra_reemplazo)
+        reemplazar_palabra_en_docx(documento_entrada,palabras_buscar,palabra_reemplazo)
